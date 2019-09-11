258457b2e9a04ece77924491f8f5d5bbdaa786aa
Coldairarrow <xgc15657519397@outlook.com>
2019-09-11 22:09:50 +0800

    Demo更新

---
+++ examples/Demo.Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using Coldairarrow.Util;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "127.0.0.1:2181";
+            new IdHelperBootstrapper()
+                .UseZookeeper(conString, 200, "Test")
+                .Boot();
+
+            Console.WriteLine($"WorkerId:{IdHelper.WorkerId}");
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            List<Task> tasks = new List<Task>();
+            BlockingCollection<string> ids = new BlockingCollection<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    for (int j = 0; j < 1000000; j++)
+                    {
+                        ids.Add(IdHelper.GetId());
+                    }
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            watch.Stop();
+            Console.WriteLine($"耗时:{watch.ElapsedMilliseconds}ms,是否有重复:{ids.Count != ids.Distinct().Count()}");
+        }
+    }
+}
--- examples/Demo.Zookeeper/Program.cs
+++ examples/Demo.Zookeeper/Program.cs
@@ -1,15 +1,15 @@
 ﻿using Coldairarrow.Util;
 using System;
 
-namespace Demo
+namespace Demo.Zookeeper
 {
     class Program
     {
         static void Main(string[] args)
         {
             new IdHelperBootstrapper()
-                //设置WorkerId
-                .SetWorkderId(1)
+                //使用Zookeeper自动分配管理WorkerId,解决时间回退问题和自动分配问题
+                .UseZookeeper("127.0.0.1:2181", 200, "Test")
                 .Boot();
 
             Console.WriteLine($"WorkerId:{IdHelper.WorkerId},Id:{IdHelper.GetId()}");
